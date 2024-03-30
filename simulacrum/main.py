bd063edf616d7595f9244025290b4a7c0df0eb71
Andrey Smirnov <smirnov.andrey.serg@gmail.com>
2024-03-30 22:07:01 +0300

    Настроен расчёт состояний сцен и передача через вебсокеты

--- simulacrum/main.py
+++ simulacrum/main.py
@@ -1,19 +1,59 @@
-from datetime import datetime
-from time import sleep
+import asyncio
+import enum
 
 import uvicorn
 from fastapi import FastAPI
-from starlette.websockets import WebSocket
+from starlette.websockets import WebSocket, WebSocketDisconnect
+
+from simulacrum.models import SceneStatement, Point, Vector, ObjectStatement
 
 app = FastAPI()
+
+FPS = 30
+
+
+class WebSocketCommand(enum.Enum):
+    start = "start"
+    pause = "pause"
+
+
+class SceneEventLoop:
+    def __init__(self, websocket: WebSocket) -> None:
+        self.websocket = websocket
+        self.state = WebSocketCommand.start
+
+    async def run(self) -> None:
+        try:
+            await self.websocket.accept()
+            await asyncio.gather(
+                self.calculate_scene_statements(),
+                self.control_buffer_capacity(),
+            )
+        except WebSocketDisconnect:
+            pass
+
+    async def calculate_scene_statements(self) -> None:
+        rotation = Point()
+        time_step = 0
+        while True:
+            rotation = rotation + Vector(x=0.1, y=0.1)
+            time_step += 1 / FPS
+            obj_statement = ObjectStatement(rotation=rotation)
+            statement = SceneStatement(objects=[obj_statement])
+            await self.websocket.send_text(statement.model_dump_json())
+
+            await asyncio.sleep(0.01)
+
+    async def control_buffer_capacity(self) -> None:
+        while True:
+            command = await self.websocket.receive_text()
+            print(f"Получена команда {command}")
+            self.state = WebSocketCommand(command)
 
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        await websocket.send_text(f"Сообщение ушло: {datetime.today()}")
-        sleep(1)
+    await SceneEventLoop(websocket).run()
 
 
 if __name__ == "__main__":
--- simulacrum/models.py
+++ simulacrum/models.py
@@ -1,5 +1,64 @@
-from pydantic import BaseModel
+from __future__ import annotations
+
+import uuid as uid
+from typing import Self
+
+from pydantic import BaseModel as PydanticModel, ConfigDict, Field
+from pydantic.alias_generators import to_camel
 
 
-class PhysicalBody(BaseModel):
-    pass
+class BaseModel(PydanticModel):
+    model_config = ConfigDict(
+        alias_generator=to_camel,
+        populate_by_name=True,
+    )
+
+
+class Point(BaseModel):
+    x: float = 0
+    y: float = 0
+    z: float = 0
+
+    def __add__(self, other: Point) -> Self:
+        return Point(
+            x=self.x + other.x,
+            y=self.y + other.y,
+            z=self.z + other.z,
+        )
+
+
+class Vector(Point):
+    ...
+
+
+class CameraStatement(BaseModel):
+    """
+    Камера сцены
+
+    :param field_of_view: Угол обзора
+    :param position: Координаты размещения камеры
+    """
+    field_of_view: float = 75
+    position: Point
+
+
+class ObjectStatement(BaseModel):
+    """
+    Состояние объекта
+
+    :param uuid: Уникальный идентификатор
+    :param time: Время с начала запуска [мс]
+    :param coordinates: Координаты в пространстве
+    :param rotation: Поворот объекта
+    """
+
+    uuid: uid.UUID = Field(default_factory=uid.uuid4)
+    time: float = 0
+
+    coordinates: Point = Field(default_factory=Point)
+    rotation: Point = Field(default_factory=Point)
+
+
+class SceneStatement(BaseModel):
+    camera: CameraStatement | None = None
+    objects: list[ObjectStatement] = Field(default_factory=list)
